20cff752d3c2a59196983d6ab8cee5a408adccdc
Jokerls <1152501640@qq.com>
2020-03-10 22:24:17 +0800

    2020-3-10 Finish the NavMenu and BreadCrumb

---
+++ src/components/v-BreadCrumb.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-breadcrumb separator="/">
+    <!-- <el-breadcrumb-item :to="{ path: '/' }">仓库管理</el-breadcrumb-item>
+    <el-breadcrumb-item>
+      <a href="/">入库</a>
+    </el-breadcrumb-item>-->
+    <el-breadcrumb-item>仓库管理</el-breadcrumb-item>
+    <el-breadcrumb-item>入库</el-breadcrumb-item>
+    <el-breadcrumb-item>入库单填写</el-breadcrumb-item>
+    <!-- <el-breadcrumb-item>活动详情</el-breadcrumb-item> -->
+  </el-breadcrumb>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+</style>
